0ea3634bf41a1d96fd674dd338e38de5d6afdbe2
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2021-10-26 05:43:58 +0700

    testing use django test client instead of manually creating request

--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,20 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolve_to_homepage_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
+
         html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do List</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_use_home_template(self):
+        response = self.client.get('/')
+
+        self.assertTemplateUsed(response, 'home.html')
