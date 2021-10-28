5267f515f33b9cce3a8c465e5f631bfe16e33898
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2021-10-28 23:15:20 +0700

    render list from db and test it using functional_tests

--- lists/tests.py
+++ lists/tests.py
@@ -36,6 +36,15 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_display_all_list_items(self):
+        Item.objects.create(text = 'itemy 1')
+        Item.objects.create(text = 'itemy 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemy 1', response.content.decode())
+        self.assertIn('itemy 2', response.content.decode())
+
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
